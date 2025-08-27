e52c51a3f740d8bea57f66d92ea72df6601d4ee4
Marcel Donges <marcel.donges@gmail.com>
2025-08-27 09:50:54 +0200

    🔧 Fix User import path in Web4Requirement - use correct dist/ts/ structure

--- components/Web4Requirement/latest/src/ts/layer2/DefaultRequirement.ts
+++ components/Web4Requirement/latest/src/ts/layer2/DefaultRequirement.ts
@@ -3,7 +3,7 @@ import * as fs from 'fs/promises';
 import * as path from 'path';
 import { fileURLToPath } from 'url';
 import { UnitIndexStorage } from '../../../../../Unit/latest/dist/ts/layer2/UnitIndexStorage.js';
-import { DefaultUser } from '../../../../../User/latest/dist/layer2/DefaultUser.js';
+import { DefaultUser } from '../../../../../User/latest/dist/ts/layer2/DefaultUser.js';
 
 // ES module equivalent of __dirname
 const __filename = fileURLToPath(import.meta.url);
