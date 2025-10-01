6dd3f2ba56f3d9994097eda7865610d0c8b6fdff
Marcel Donges <marcel.donges@gmail.com>
2025-10-01 17:08:13 +0200

    template update

--- components/Web4TSComponent/0.3.2.0/src/sh/install-deps.sh
+++ components/Web4TSComponent/0.3.2.0/src/sh/install-deps.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-echo "📦 Installing dependencies at project root..."
-cd ../../../ && npm install
-
 echo "🔗 Creating symlink to shared node_modules..."
-cd components/Web4TSComponent/0.3.0.10
 ln -sf ../../../node_modules node_modules
+
+echo "📦 Installing component dependencies..."
+npm install
--- components/Web4TSComponent/0.3.2.0/src/sh/test.sh
+++ components/Web4TSComponent/0.3.2.0/src/sh/test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "🧪 Running Web4TSComponent tests..."
 
-# Prepare (clean, install, build)
+# Smart build before testing
 ./src/sh/build.sh
 
 # Run tests
