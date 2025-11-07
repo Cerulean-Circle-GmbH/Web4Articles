6853c23624c6befe360a9baaa5566fba4acdea44
Marcel Donges <marcel.donges@gmail.com>
2025-11-07 17:17:53 +0100

    2025-11-07-UTC-0000.eliminate-path-duplication-all-cases.pdca.md - TRUE Radical OOP: Eliminate target variable from testItCase() method (6/7)

--- components/Web4TSComponent/0.3.18.3/src/ts/layer2/DefaultWeb4TSComponent.ts
+++ components/Web4TSComponent/0.3.18.3/src/ts/layer2/DefaultWeb4TSComponent.ts
@@ -2392,8 +2392,6 @@ export class DefaultWeb4TSComponent implements Web4TSComponent {
     console.log(`   Describe: ${targetDescribe.name}`);
     console.log(`   File: ${targetFile.name}`);
     
-    // ✅ RADICAL OOP: Use target instance for component root
-    const target = this.model.context || this;
     // @pdca 2025-11-07-UTC-0000.eliminate-path-duplication-all-cases.pdca.md - Use pre-calculated paths
     const componentRoot = this.model.targetComponentRoot!;
     
