a3756c193b52ea88c228b8d76f1252bba46efdba
Marcel Donges <marcel.donges@gmail.com>
2025-11-07 11:28:23 +0100

    merged PDCA 0.3.18.1 into dev/0400

--- components/PDCA/0.3.18.1/src/sh/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "🧹 Cleaning all artifacts..."
-rm -rf dist
-rm -rf node_modules
-# Note: Global node_modules at project root is NOT cleaned by component clean
-# Use 'npm run clean:global' at project root to clean global node_modules
