a77a503363d6351239b838ad5af738f1e461e856
Hannes Nortje <nortje.johannes@gmail.com>
2025-11-10 16:16:01 +0100

    fix: Add missing execSync import to rewritePDCA tests
    - Tests TC-REWRITE-HISTORY-01 through 04 use execSync for git commands
    - Missing import caused ReferenceError

--- components/PDCA/0.3.6.2/test/pdca.rewritePDCA.test.ts
+++ components/PDCA/0.3.6.2/test/pdca.rewritePDCA.test.ts
@@ -8,6 +8,7 @@ import { DefaultPDCA } from '../src/ts/layer2/DefaultPDCA.js';
 import * as fs from 'fs';
 import * as path from 'path';
 import { fileURLToPath } from 'url';
+import { execSync } from 'child_process';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
